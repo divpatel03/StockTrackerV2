cff3084a3b07ac19e9e8229ff0e0731c6076b35d
Divya Patel <divpatel031000@gmail.com>
2024-06-10 13:50:18 -0400

    Added router to main

--- my-react-app/src/main.jsx
+++ my-react-app/src/main.jsx
@@ -3,8 +3,21 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Hello world!</div>,
+  },
+]);
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <RouterProvider router={router} />
     <App />
   </React.StrictMode>,
 )
