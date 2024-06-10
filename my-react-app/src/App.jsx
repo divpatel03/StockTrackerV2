9d6e81ce5a45c135e2dd8ac1d574be5b9614a7fe
Divya Patel <divpatel031000@gmail.com>
2024-06-09 23:00:19 -0400

    Added login

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,7 +1,24 @@
 import React, { useState } from 'react';
 
 function App() {
- 
+    const [name, setName] = useState("");
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+    }
+
+    let isLoggedIn = false;
+
+    
+    return(
+        <div className="Login">
+            <h1>Login</h1>
+            <form>
+            <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
+            <input type="submit"/>
+            </form>
+        </div>
+    );
 }
 
 export default App;
--- my-react-app/src/Login.jsx
+++
@@ -1,7 +0,0 @@
-import React, { useState } from 'react';
-
-function Login() {
-
-}
-
-export default Login;
