e1f24916de69682b786ee29abe170c4a23751dc4
Divya Patel <divpatel031000@gmail.com>
2024-06-10 14:40:07 -0400

    Added html css styling

--- my-react-app/src/Dashboard.jsx
+++ my-react-app/src/Dashboard.jsx
@@ -1,15 +1,32 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
 
 function Dashboard() {
     const location = useLocation();
     const { username } = location.state || {};
 
+    const [currTime, setCurrTime] = useState(new Date().toLocaleTimeString());
+
+    useEffect(() => {
+        const intervalId = setInterval(() => {
+            setCurrTime(new Date().toLocaleTimeString());
+        }, 1000); // Update every second
+
+        // Cleanup interval on component unmount
+        return () => clearInterval(intervalId);
+    }, []);
+
     return (
-        <div>
-            <h1>Dashboard</h1>
-            <p>Welcome {username}!</p>
+        <div className="flex-container">
+            <div className = "leftSide">
+            <h1>{username}</h1>
+            <h3>Current Time (EST): {currTime}</h3>
         </div>
+            <div className = "rightSide">
+                
+            </div>
+        </div>
+        
     );
 }
 
