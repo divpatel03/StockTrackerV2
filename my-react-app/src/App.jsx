2379dd74a59316f93fbbf7b0bff7615cfe3109fd
Divya Patel <divpatel031000@gmail.com>
2024-06-10 13:43:04 -0400

    Added the username check

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,18 +1,27 @@
 import React, { useState } from 'react';
 
 function App() {
-    let isLoggedIn = false;
+    const [loggedIn, setIsLoggedIn] = useState(false);
     const [name, setName] = useState("");
 
     const handleSubmit = (event) => {
         event.preventDefault();
-        alert("Form Submitted");
+
+        if(name.length === 0) {
+            alert("ERROR: Username must be 1 character or longer.");
+        }
+
+        else {
+            alert("Logged In!");
+            setIsLoggedIn(true);
+        }
     }
+  
 
     return(
         <div className="Login">
             <h1>Login</h1>
-            <form>
+            <form onSubmit={handleSubmit}>
             <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
             <input type="submit"/>
             </form>
