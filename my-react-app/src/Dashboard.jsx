526f111abbb4a61a20ab685c9c4a659a3a4cedcf
Divya Patel <divpatel031000@gmail.com>
2024-06-10 15:04:22 -0400

    Added market open or closed alerts

--- my-react-app/src/Dashboard.jsx
+++ my-react-app/src/Dashboard.jsx
@@ -6,11 +6,21 @@ function Dashboard() {
     const { username } = location.state || {};
 
     const [currTime, setCurrTime] = useState(new Date().toLocaleTimeString());
+    const [marketMessage, setMarketMessage] = useState("");
 
     useEffect(() => {
         const intervalId = setInterval(() => {
             setCurrTime(new Date().toLocaleTimeString());
         }, 1000); // Update every second
+
+        const now = new Date();
+        const hour = now.getHours();
+        if(hour <= 16 && hour >= 9.5) {
+            setMarketMessage("Market: Open");
+        }
+        else {
+            setMarketMessage("Market Closed");
+        }
 
         // Cleanup interval on component unmount
         return () => clearInterval(intervalId);
@@ -23,7 +33,7 @@ function Dashboard() {
             <h3>Current Time (EST): {currTime}</h3>
         </div>
             <div className = "rightSide">
-                
+                <h2><p>{marketMessage}</p></h2>
             </div>
         </div>
         
